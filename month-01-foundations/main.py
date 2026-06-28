cd9e00f703e26137249b9ef3e3b4c4dfe419ac41
Adil Khan <adilkha2428@gmail.com>
2026-06-28 08:55:43 +0500

    Remove old month-01-foundations - restructuring project

--- month-01-foundations/main.py
+++
@@ -1,4 +0,0 @@
-from utils import validate_email
-
-print(validate_email("adil@test.com"))
-print(validate_email("not-an-email"))
--- month-01-foundations/utils.py
+++
@@ -1,7 +0,0 @@
-# utils.py
-
-
-def validate_email(email):
-    if "@" not in email or "." not in email:
-        return False
-    return True
